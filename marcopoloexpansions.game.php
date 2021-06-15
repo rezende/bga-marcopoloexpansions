29c5aacc0f4858124d04f0bdd5aae786c39b56a7
Rezende <smugskull@pm.me>
2021-06-15 02:29:31 -0300

    Remove dump statements

--- marcopoloexpansions.game.php
+++ marcopoloexpansions.game.php
@@ -425,16 +425,11 @@ class MarcoPoloExpansions extends Table
         $valid_city_bonus_types = array_filter($this->city_bonus_types, array("MarcoPoloExpansions", "filterExpansionFromMaterialTypes"));
         if (self::getGameStateValue("expert_variant") == 1) //use random
         {
-            self::dump('VALID_CITY_BONUS', $valid_city_bonus_types);
             $required_city_bonus = array_values(array_filter($valid_city_bonus_types, function($b) { return $b['type'] == 3;}));
-            self::dump('REQUIRED_CITY_BONUS', $required_city_bonus);
             $other_city_bonus = array_values(array_filter($valid_city_bonus_types, function($b) { return $b['type'] != 3;}));
             shuffle($other_city_bonus);
-            self::dump('OTHER_CITY_BONUS SHUFFLED', $other_city_bonus);
-            $picked_city_bonuses = array_slice($other_city_bonus, 0, 5);
-            self::dump('PICKED_CITY_BONUS SLICED', $picked_city_bonuses);
+            $picked_city_bonuses = array_slice($other_city_bonus, 0, 5); // 5 considering only main map
             $picked_city_bonuses = array_merge($required_city_bonus, $other_city_bonus);
-            self::dump('PICKED_CITY_BONUS REQUIRED ADDED', $picked_city_bonuses);
             $this->randomlyAssignBonusPieces('city_bonus', $picked_city_bonuses, "small_city");
         } else {
             $sql = "INSERT INTO piece (piece_type, piece_type_arg, piece_location, piece_location_arg) VALUES";
