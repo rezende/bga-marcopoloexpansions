4d6a5e6df961ad97772af541e90563bd35a418cb
Rezende <smugskull@pm.me>
2021-06-19 19:48:20 -0300

    Implement arghun_card_set opt

--- marcopoloexpansions.game.php
+++ marcopoloexpansions.game.php
@@ -485,9 +485,37 @@ class MarcoPoloExpansions extends Table
         }
     }
 
+    function getArghunCardIds(bool $arghun_hand = true)
+    {
+        $set_no = self::getGameStateValue("arghun_card_set");
+        // assert $set_no > 0
+        $all_ids = range(0, 30);
+        if ($set_no > 5) {
+            $arghun_ids = range(25, 30);
+        } else {
+            $arghun_ids = range(6 * ($set_no - 1), 6 * $set_no - 1);
+        }
+        if ($arghun_hand === true) {
+            return $arghun_ids;
+        }
+        return array_diff($all_ids, $arghun_ids);
+    }
+
     function assignCityCards()
     {
-        $this->randomlyAssignBonusPieces('city_card', $this->city_card_types, "large_city");      //TODO valid city card in other expansion
+        $city_cards = $this->city_card_types;
+        if (self::getGameStateValue("arghun_card_set") > 0) {
+            $city_cards = array_filter(
+                $this->city_card_types,
+                function ($city_card) {
+                    return in_array(
+                        $city_card['type'],
+                        $this->getArghunCardIds(false)
+                    );
+                }
+            );
+        }
+        $this->randomlyAssignBonusPieces('city_card', $city_cards, "large_city");
     }
 
     function assignOutpostBonuses()
@@ -497,8 +525,17 @@ class MarcoPoloExpansions extends Table
 
     function presetupCharacter($character_type)
     {
-        if ($character_type == self::CHARACTER_KHAN_ARGHUN)
-        {
+        if ($character_type == self::CHARACTER_KHAN_ARGHUN) {
+            if (self::getGameStateValue("arghun_card_set") > 0) {
+                $arghun_cards = $this->getArghunCardIds();
+                foreach ($arghun_cards as $card_type_id) {
+                    self::DbQuery(
+                        "INSERT INTO piece (piece_type, piece_type_arg, piece_location)
+                        VALUES ('city_card', '{$card_type_id}', 'pick_character')"
+                    );
+                }
+                return;
+            }
             $current_count = 0;
             $shuffled_card_index = 0;
             $current_city_cards = self::getObjectListFromDB(
